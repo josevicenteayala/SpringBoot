c65d8dd0ac4c4fa10019d668eac01a7cbcfaa745
josevicenteayalaluna <josevicenteayalaluna@192.168.1.71>
2018-02-25 06:33:06 -0500

    first commit

---
+++ SpringBootProject/src/main/java/co/com/intergrupo/maven/boot/SpringBootProjectApplication.java
@@ -0,0 +1,12 @@
+package co.com.intergrupo.maven.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+}
