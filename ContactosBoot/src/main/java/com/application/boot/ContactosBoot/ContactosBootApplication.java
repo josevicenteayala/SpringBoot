ec6bf813f055dbc9192b7e7ce05d7a114e0311a5
DESKTOP-4LLK40V\Asus <jayala@intergrupo.com>
2018-03-02 00:37:02 -0500

    Add another project

---
+++ ContactosBoot/src/main/java/com/application/boot/ContactosBoot/ContactosBootApplication.java
@@ -0,0 +1,12 @@
+package com.application.boot.ContactosBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactosBootApplication.class, args);
+	}
+}
